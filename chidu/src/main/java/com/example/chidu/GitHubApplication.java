1a7a518d4ad443c1c8470fb167df16859e26b649
chidanand-github <chidu.k008@gmail.com>
2020-11-05 13:17:56 +0530

    commit the code

---
+++ chidu/src/main/java/com/example/chidu/GitHubApplication.java
@@ -0,0 +1,35 @@
+package com.example.chidu;
+
+import java.io.ObjectInputStream.GetField;
+import java.net.HttpRetryException;
+import java.nio.file.Paths;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(GitHubApplication.class, args);
+		System.out.println("chidanand kadapure");
+		String repoUrl = "https://github.com/chidanand-github/sampleRepo.git";
+		String cloneDirectoryPath = "git/chiduRepo/code"; // Ex.in windows c:\\gitProjects\SpringBootMongoDbCRUD\
+		try {
+		    System.out.println("Cloning "+repoUrl+" into "+repoUrl);
+		    Git.cloneRepository()
+		        .setURI(repoUrl)
+		        .setDirectory(Paths.get(cloneDirectoryPath).toFile())
+		        .call();
+		    System.out.println("Completed Cloning");
+		} catch (GitAPIException e) {
+		    System.out.println("Exception occurred while cloning repo");
+		    e.printStackTrace();
+		}
+	
+	}
+
+}
